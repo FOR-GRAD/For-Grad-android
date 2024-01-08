b20a79a433050cd3de8572b51eac86fae7e43a76
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2024-01-08 21:53:21 +0900

    Docs:implementation 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,8 @@ dependencies {
 	testImplementation("junit:junit:4.13.2")
 	androidTestImplementation("androidx.test.ext:junit:1.1.5")
 	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+	//border radius
+	implementation ("com.google.android.material:material:1.0.0")
+	//bottom Sheet Dialog
+	implementation ("com.google.android.material:material:1.6.1")
 }
