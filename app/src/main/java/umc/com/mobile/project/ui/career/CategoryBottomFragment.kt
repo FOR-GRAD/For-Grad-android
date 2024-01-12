8aed8c8f4cd9fbfdfc9c910868ba2e5dc2825845
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2024-01-12 16:03:56 +0900

    Design: frag_categoryBottom 추가

---
+++ app/src/main/java/umc/com/mobile/project/ui/career/CategoryBottomFragment.kt
@@ -0,0 +1,51 @@
+package umc.com.mobile.project.ui.career
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.NumberPicker
+import androidx.fragment.app.viewModels
+import umc.com.mobile.project.databinding.FragmentCategoryBottomBinding
+
+class CategoryBottomFragment : Fragment() {
+    private var _binding: FragmentCategoryBottomBinding? = null
+    private val viewModel: CategoryBottomViewModel by viewModels()
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCategoryBottomBinding.inflate(inflater, container, false)
+
+        /*viewModel.text.observe(viewLifecycleOwner) {
+            binding.textCareer.text = it
+        }*/
+
+        val categories = arrayOf(
+            getString(umc.com.mobile.project.R.string.career_certificate_title),
+            getString(umc.com.mobile.project.R.string.career_contest_title),
+            getString(umc.com.mobile.project.R.string.career_volunteer_title),
+            "교외활동",
+            "기타"
+        )
+
+        var numberPicker: NumberPicker = _binding!!.npCategoryBottomNumberPicker
+        numberPicker.minValue = 0
+        numberPicker.maxValue = categories.size - 1
+
+        val stringArray = categories.map { it as String? }.toTypedArray()
+        numberPicker.displayedValues = stringArray
+        numberPicker.wrapSelectorWheel = false
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
