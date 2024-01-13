21a96d1f9f9beae66084c6c4a920d6ca358ac42c
dkyuuum <dkyuuum@gmail.com>
2024-01-14 02:02:15 +0900

    Feat: 현재 날짜 적용

---
+++ app/src/main/java/umc/com/mobile/project/ui/board/viewmodel/GradDateViewModel.kt
@@ -0,0 +1,20 @@
+package umc.com.mobile.project.ui.board.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class GradDateViewModel : ViewModel() {
+    val selectedDate = MutableLiveData<String>()
+	val cheeringMemo = MutableLiveData<String>()
+
+	fun getData(): MutableLiveData<String> = cheeringMemo
+	fun updateMemo(input: String) {
+		cheeringMemo.value = input
+	}
+
+}
--- app/src/main/java/umc/com/mobile/project/ui/home/HomeFragment.kt
+++ app/src/main/java/umc/com/mobile/project/ui/home/HomeFragment.kt
@@ -2,20 +2,24 @@ package umc.com.mobile.project.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import umc.com.mobile.project.R
 import umc.com.mobile.project.databinding.FragmentHomeBinding
 import umc.com.mobile.project.ui.board.GradDateFragment
+import umc.com.mobile.project.ui.board.viewmodel.GradDateViewModel
 import umc.com.mobile.project.ui.common.NavigationUtil.navigate
 import umc.com.mobile.project.ui.home.viewmodel.HomeViewModel
 
 class HomeFragment : Fragment() {
 	private var _binding: FragmentHomeBinding? = null
 	private val viewModel: HomeViewModel by viewModels()
+	private val gradDateViewModel: GradDateViewModel by viewModels()
 	private val binding get() = _binding!!
 
 	override fun onCreateView(
@@ -25,9 +29,8 @@ class HomeFragment : Fragment() {
 	): View {
 		_binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-		binding.btnCheeringWordMove.setOnClickListener {
-			navigate(R.id.action_fragment_home_to_fragment_date)
-		}
+		navigateGradDateFragment() // 졸업 예정일로 이동
+		saveCheeringMemo() // 응원의 한마디
 
 		return binding.root
 	}
@@ -35,5 +38,19 @@ class HomeFragment : Fragment() {
 	override fun onDestroyView() {
 		super.onDestroyView()
 		_binding = null
+	}
+
+	private fun navigateGradDateFragment() {
+		binding.btnCheeringWordMove.setOnClickListener {
+			navigate(R.id.action_fragment_home_to_fragment_date)
+		}
+	}
+
+	private fun saveCheeringMemo() {
+		gradDateViewModel.updateMemo(binding.tvCheeringWord.text.toString())
+
+		gradDateViewModel.getData().observe(viewLifecycleOwner, Observer {
+			binding.tvCheeringWord.text = it
+		})
 	}
 }
