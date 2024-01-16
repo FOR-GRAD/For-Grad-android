fdbb7f78a075b3b320b6936776277b4292efae8e
dkyuuum <dkyuuum@gmail.com>
2024-01-17 00:01:24 +0900

    Feat: 응원의 한마디 연결

--- app/src/main/java/umc/com/mobile/project/ui/home/HomeFragment.kt
+++ app/src/main/java/umc/com/mobile/project/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import umc.com.mobile.project.R
@@ -20,7 +21,7 @@ import umc.com.mobile.project.ui.home.viewmodel.HomeViewModel
 class HomeFragment : Fragment() {
 	private var _binding: FragmentHomeBinding? = null
 	private val viewModel: HomeViewModel by viewModels()
-	private val gradDateViewModel: GradDateViewModel by viewModels()
+	private val gradDateViewModel: GradDateViewModel by activityViewModels()
 	private val binding get() = _binding!!
 
 	override fun onCreateView(
