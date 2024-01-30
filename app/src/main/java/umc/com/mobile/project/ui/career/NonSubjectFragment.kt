ac910d095982b15c6a6cdaeb106b17cd4909f31e
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2024-01-30 18:43:26 +0900

    Feat: 비교과 포인트 api 연결 성공, 페이징 실패

--- app/src/main/java/umc/com/mobile/project/ui/career/NonSubjectFragment.kt
+++ app/src/main/java/umc/com/mobile/project/ui/career/NonSubjectFragment.kt
@@ -1,13 +1,16 @@
 package umc.com.mobile.project.ui.career
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import umc.com.mobile.project.R
+import umc.com.mobile.project.data.network.api.CareerApi
 import umc.com.mobile.project.databinding.FragmentCareerNonsubjectBinding
 import umc.com.mobile.project.ui.career.adapter.NonSubjectRVAdapter
 import umc.com.mobile.project.ui.career.data.CertificateDto
@@ -32,19 +35,47 @@ class NonSubjectFragment : Fragment() {
             CertificateDto("2022-02-15", "자격증4", "필기", "1급"),
             CertificateDto("2022-03-30", "자격증5", "실기", "1급")
         )
-        val adapter = NonSubjectRVAdapter()
-        binding.rvCareerNonsubjectList.adapter = adapter
-        binding.rvCareerNonsubjectList.layoutManager = LinearLayoutManager(requireContext())
 
         _binding!!.ivCareerNonsubjectBack.setOnClickListener {
             navigate(R.id.action_fragment_nonsubject_to_fragment_career)
         }
-
-        viewModel.certificates.observe(viewLifecycleOwner, { pagingData ->
-            adapter.submitData(viewLifecycleOwner.lifecycle, pagingData)
+        viewModel.getNonSubjectInfo()// 비교과 조회 api
+        //비교과 포인트 띄우기
+        viewModel.nonSubjectInfo.observe(viewLifecycleOwner, { nonSubjectResponse ->
+            nonSubjectResponse?.result?.pointSummaryDto?.let { pointSummaryDto ->
+                binding.etCareerNonsubjectContent1.text = Editable.Factory.getInstance().newEditable(pointSummaryDto.semesterPoints)
+                binding.etCareerNonsubjectContent2.text = Editable.Factory.getInstance().newEditable(pointSummaryDto.carryoverPoints)
+                binding.etCareerNonsubjectContent3.text = Editable.Factory.getInstance().newEditable(pointSummaryDto.accumulatedPoints)
+                binding.etCareerNonsubjectContent4.text = Editable.Factory.getInstance().newEditable(pointSummaryDto.graduationPoints)
+            }
+            nonSubjectResponse?.result.let { result ->
+                val adapter = NonSubjectRVAdapter(result)
+                binding.rvCareerNonsubjectList.adapter = adapter
+                binding.rvCareerNonsubjectList.layoutManager = LinearLayoutManager(requireContext())
+            }
         })
 
-        viewModel.getNonSubjectInfo() // 비교과 조회 api
+/*        viewModel.certificates.observe(viewLifecycleOwner, { pagingData ->
+            adapter.submitData(viewLifecycleOwner.lifecycle, pagingData)
+        })*/
+
+/*        _binding!!.rvCareerNonsubjectList.apply {
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val lastVisibleItemPosition =
+                        (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition()
+                    val itemTotalCount = recyclerView.adapter!!.itemCount
+                    if (lastVisibleItemPosition >= itemTotalCount - 1) {
+                        if (page * 10 < totalItemCount) {
+                            page++
+                            requestDTO.pages = page
+                            myFunction(requestDTO)
+                        }
+                    }
+                }
+            })
+        }*/
         return binding.root
     }
 
--- app/src/main/java/umc/com/mobile/project/ui/career/adapter/NonSubjectRVAdapter.kt
+++ app/src/main/java/umc/com/mobile/project/ui/career/adapter/NonSubjectRVAdapter.kt
@@ -7,11 +7,13 @@ import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import umc.com.mobile.project.data.model.career.PointDtoList
+import umc.com.mobile.project.data.model.career.Result
 import umc.com.mobile.project.databinding.ItemCertificateBinding
 import umc.com.mobile.project.ui.career.data.CertificateDto
 
-/*
-class NonSubjectRVAdapter(private val nonsubjectList: ArrayList<CertificateDto>): RecyclerView.Adapter<NonSubjectRVAdapter.NonSubjectViewHolder>(){
+class NonSubjectRVAdapter(private val response: Result?): RecyclerView.Adapter<NonSubjectRVAdapter.NonSubjectViewHolder>(){
+    private val nonsubjectList: ArrayList<PointDtoList> = response?.pointDtoList as ArrayList<PointDtoList>
 
     override fun getItemCount(): Int {
         return nonsubjectList?.size ?: 0
@@ -27,8 +29,8 @@ class NonSubjectRVAdapter(private val nonsubjectList: ArrayList<CertificateDto>)
 
         holder.itemBinding.etCertificateDate.text = editableFactory.newEditable(nonsubjectList[position].date)
         holder.itemBinding.etCertificateTitle.text = editableFactory.newEditable(nonsubjectList[position].title)
-        holder.itemBinding.etCertificateType.text = editableFactory.newEditable(nonsubjectList[position].type)
-        holder.itemBinding.etCertificateRating.text = editableFactory.newEditable(nonsubjectList[position].rating)
+        holder.itemBinding.etCertificateType.text = editableFactory.newEditable(nonsubjectList[position].rewardPoints)
+        holder.itemBinding.etCertificateRating.text = editableFactory.newEditable(nonsubjectList[position].accumulatedPoints)
     }
 
     class NonSubjectViewHolder(val itemBinding: ItemCertificateBinding) : RecyclerView.ViewHolder(itemBinding.root)
@@ -42,7 +44,8 @@ class NonSubjectRVAdapter(private val nonsubjectList: ArrayList<CertificateDto>)
             return oldItem == newItem
         }
     }
-}*/
+}
+/*
 class NonSubjectRVAdapter :
     PagingDataAdapter<CertificateDto, NonSubjectRVAdapter.NonSubjectViewHolder>(DiffCallback()) {
 
@@ -85,4 +88,4 @@ class NonSubjectRVAdapter :
             return oldItem == newItem
         }
     }
-}
+}*/
