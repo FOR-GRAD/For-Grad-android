8c98daad0dda785d87f233f1700de512735cbbb4
kimyujin-com <80165026+kimyujin-com@users.noreply.github.com>
2024-02-11 22:19:01 +0900

    Fix: 커밋 충돌 해결

--- app/src/main/java/umc/com/mobile/project/ui/board/GradDateFragment.kt
+++ app/src/main/java/umc/com/mobile/project/ui/board/GradDateFragment.kt
@@ -3,6 +3,7 @@ package umc.com.mobile.project.ui.board
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,16 +11,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import umc.com.mobile.project.R
 import umc.com.mobile.project.databinding.FragmentGradDateBinding
 import umc.com.mobile.project.databinding.FragmentGradDateBottomBinding
 import umc.com.mobile.project.ui.board.viewmodel.GradDateViewModel
 import umc.com.mobile.project.ui.common.NavigationUtil.navigate
-import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.Locale
 
 class GradDateFragment : Fragment() {
 	private var _binding: FragmentGradDateBinding? = null
@@ -49,57 +49,27 @@ class GradDateFragment : Fragment() {
 			}
 		}
 
+		//실패 코드-값을 눌러도 text 안 바뀜
+		viewModel.selectedDate.observe(viewLifecycleOwner) { selectedDate ->
+			binding.tvGradDateDate.text = selectedDate
+		}
+
 		navigateBack() // 뒤로 가기 버튼 클릭 시
 		initTodayDate() // 오늘 날짜 설정
+		saveCheeringMemo()
+		viewModel.getDateInfo()
 
-		viewModel.dateResponse.observe(viewLifecycleOwner, Observer { response ->
-			if (response != null && response.result != null) {
-				binding.tvGradDateDday.text = "D-" + response.result.dday.toString()
-			}
-		})
-
-		binding.btnSave.setOnClickListener {
-			val originalFormat = SimpleDateFormat("yyyy-M월-d", Locale.KOREA)
-			val targetFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
-
-			val date = originalFormat.parse(viewModel.selectedDateRequest.value)
-			val formattedDate = targetFormat.format(date)
-			viewModel.updateCheeringMessage(binding.tvGradDateMemo.text.toString())
-			viewModel.updateDateInfo(formattedDate)
-			Toast.makeText(context, "저장되었습니다.", Toast.LENGTH_LONG).show()
-			navigate(R.id.action_fragment_date_to_fragment_home)
+		bottomSheetBinding!!.npGradDateYear.setOnClickListener {
+			updateSelectedDate()
+		}
+		bottomSheetBinding!!.npGradDateMonth.setOnClickListener {
+			updateSelectedDate()
+		}
+		bottomSheetBinding!!.npGradDateDay.setOnClickListener {
+			updateSelectedDate()
 		}
 
 		return binding.root
-	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-
-		viewModel.getDateInfo()
-
-		viewModel._selectedDate.observe(viewLifecycleOwner) { date ->
-			binding.tvGradDateDate.text = date
-		}
-
-		viewModel.dateResponse.observe(viewLifecycleOwner, Observer { response ->
-			if (response != null && response.result != null) {
-				val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
-				val outputFormat = SimpleDateFormat("졸업 예정일 yyyy'년' MM'월' dd'일'", Locale.KOREA)
-
-				val dateStr = inputFormat.parse(response.result.gradDate)
-				if (dateStr != null) {
-					val formattedDateStr = outputFormat.format(dateStr)
-					binding.tvGradDateDate.text = formattedDateStr
-				}
-
-				binding.tvGradDateDday.text = "D-" + response.result.dday.toString()
-			}
-		})
-
-		viewModel.cheeringMessage.observe(viewLifecycleOwner) { message ->
-			binding.tvGradDateMemo.text = Editable.Factory.getInstance().newEditable(message)
-		}
 	}
 
 	override fun onDestroyView() {
@@ -120,4 +90,21 @@ class GradDateFragment : Fragment() {
 		binding.tvGradDateToday.text = currentTime.format(pattern)
 	}
 
+	private fun saveCheeringMemo() {
+		binding.btnSave.setOnClickListener {
+			viewModel.updateDateInfo(binding.tvGradDateToday.text.toString())
+
+			Toast.makeText(context, "저장되었습니다.", Toast.LENGTH_LONG).show()
+		}
+	}
+
+	private fun updateSelectedDate() {
+		val selectedYear = bottomSheetBinding?.npGradDateYear?.value
+		val selectedMonth = bottomSheetBinding?.npGradDateMonth?.value
+		val selectedDay = bottomSheetBinding?.npGradDateDay?.value
+
+		val selectedDateString = "$selectedYear 년 $selectedMonth 월 $selectedDay 일"
+
+		binding.tvGradDateDate.text = selectedDateString
+	}
 }
