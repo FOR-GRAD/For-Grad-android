50602cb2308dc3d9a47febf14bbd691b6ccae53a
dkyuuum <dkyuuum@gmail.com>
2024-02-16 16:05:47 +0900

    Feat: 로그아웃 시 로그인에서 모달 띄우기

--- app/src/main/java/umc/com/mobile/project/ui/login/LoginActivity.kt
+++ app/src/main/java/umc/com/mobile/project/ui/login/LoginActivity.kt
@@ -2,15 +2,18 @@ package umc.com.mobile.project.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import umc.com.mobile.project.MainActivity
+import umc.com.mobile.project.R
 import umc.com.mobile.project.databinding.ActivityLoginBinding
 import umc.com.mobile.project.ui.login.viewmodel.LoginViewModel
 import kotlin.coroutines.coroutineContext
@@ -56,6 +59,27 @@ class LoginActivity : AppCompatActivity() {
 			}
 			false
 		}
+
+		// 이전 화면에서 로그아웃을 통해 돌아온 경우 다이얼로그 표시
+		val isLoggedOut = intent.getBooleanExtra("logged_out", false)
+		if (isLoggedOut) {
+			showLogoutDialog()
+		}
+	}
+
+	private fun showLogoutDialog() {
+		var dialogBuilder =
+			AlertDialog.Builder(this, R.style.CustomAlertDialogTheme)
+
+		val customView = layoutInflater.inflate(R.layout.custom_dialog_logout_home, null)
+		dialogBuilder.setView(customView)
+
+		val dialog = dialogBuilder.create()
+		dialog.show()
+
+		Handler().postDelayed({
+			dialog.dismiss()
+		}, 2000)
 	}
 
 }
